ce07fde931447cc5f5267af8289e92c70b75b4ca
luxiaoxun <luxiaoxun001@163.com>
2015-03-15 14:25:46 +0800

    Add log for HeartBeatHandler

--- src/com/nettymq/server/HeartBeatHandler.java
+++ src/com/nettymq/server/HeartBeatHandler.java
@@ -1,5 +1,8 @@
 package com.nettymq.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.timeout.IdleState;
@@ -10,6 +13,9 @@ import io.netty.handler.timeout.IdleStateEvent;
  */
 public class HeartBeatHandler extends ChannelInboundHandlerAdapter{
 
+	private static final Logger log = LoggerFactory
+			.getLogger(HeartBeatHandler.class);
+	
 	@Override
 	public void userEventTriggered(ChannelHandlerContext ctx, Object evt)
 			throws Exception {
@@ -17,8 +23,9 @@ public class HeartBeatHandler extends ChannelInboundHandlerAdapter{
 			IdleStateEvent event = (IdleStateEvent) evt;
 			if (event.state() == IdleState.READER_IDLE) {
 				// Read timeout
-				System.out.println("READER_IDLE: read timeout from "+ctx.channel().remoteAddress());
+				//System.out.println("READER_IDLE: read timeout from "+ctx.channel().remoteAddress());
 				//ctx.disconnect(); //Channel disconnect
+				log.info("READER_IDLE: read timeout from "+ctx.channel().remoteAddress());
 			}
 		}
 	}
