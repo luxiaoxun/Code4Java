98b08cfee4bd91e9eae6e19af256aa2ae0a7fb35
luxiaoxun <luxiaoxun001@qq.com>
2020-09-04 08:51:01 +0800

    Add Data index app

--- DataIndexApp/src/main/java/com/luxx/index/app/DataIndexApp.java
+++ DataIndexApp/src/main/java/com/luxx/index/app/DataIndexApp.java
@@ -1,11 +1,13 @@
-package com.luxx.index;
+package com.luxx.index.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @SpringBootConfiguration
+@ComponentScan(basePackages = "com.luxx.index")
 public class DataIndexApp {
     public static void main(String[] args) {
         SpringApplication.run(DataIndexApp.class, args);
--- DataIndexApp/src/main/java/com/luxx/index/app/executor/MainExecutor.java
+++ DataIndexApp/src/main/java/com/luxx/index/app/executor/MainExecutor.java
@@ -1,5 +1,6 @@
-package com.luxx.index.executor;
+package com.luxx.index.app.executor;
 
+import com.luxx.index.executor.DataIndexExecutor;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
