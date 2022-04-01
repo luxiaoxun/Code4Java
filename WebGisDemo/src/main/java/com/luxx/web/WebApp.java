7da543b6ace93bebfed7136adcd4d241e1b0116b
luxiaoxun <luxiaoxun001@qq.com>
2022-04-01 15:19:34 +0800

    Update maven dependency

--- WebGisDemo/src/main/java/com/luxx/web/WebApp.java
+++ WebGisDemo/src/main/java/com/luxx/web/WebApp.java
@@ -4,12 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @SpringBootConfiguration
+@EnableWebMvc
 @ComponentScan(basePackages = "com.luxx")
-public class WebApp {
+public class WebApp implements WebMvcConfigurer {
     public static void main(String[] args) {
         SpringApplication.run(WebApp.class, args);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
