b8e92a24227e7d19ab86065ad2fe1ad65464d79e
luxiaoxun <luxiaoxun001@qq.com>
2022-10-09 10:19:42 +0800

    Update gis map demo

--- WebGisDemo/src/main/java/com/luxx/gis/controller/MapController.java
+++ WebGisDemo/src/main/java/com/luxx/gis/controller/MapController.java
@@ -1,11 +1,11 @@
 package com.luxx.gis.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 @Slf4j
 public class MapController {
 
